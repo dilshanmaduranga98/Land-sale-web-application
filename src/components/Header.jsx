551d412a430b0e42c89e59a03a2a11f4c3012aa9
dilshanmaduranga98 <gamagedilshan98@gmail.com>
2023-05-22 00:22:25 +0530

    create half of signin page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,12 @@ import { useLocation,useNavigate} from 'react-router-dom'
 
 export default function Header() {
 
-  const location = useLocation();
-  const navigate =  useNavigate();
+  const location = useLocation();  //----- get current location of page-----//
+  const navigate =  useNavigate();  //----- navigate to between pages ----//
 
-function pathMatchRoute(route)
+
+  //---- this function use to show what is current location in navigation bar -----//
+function pathMatchRoute(route)   
 {
   if(route === location.pathname)
   {
@@ -16,24 +18,25 @@ function pathMatchRoute(route)
 }
 
 
+
   return (
     <div className="sticky top-0 z-50 bg-white shadow bgborder-b">
         <header className='flex items-center justify-between px-1 mx-auto max-w-7xl '>
-                <div className='py-3'>
-                    <img src="/araliya-logo.png" alt='araliya_logo' className='h-8 cursor-pointer' onClick={()=>navigate("/")}/>
+                <div className='flex items-center py-3'>
+                    <img src="/araliya-logo.png" alt='araliya_logo' className='h-10 cursor-pointer' onClick={()=>navigate("/")}/>
                 </div>
                 <div>
                     <ul className='flex space-x-12'>
                       
-                        <li className={`cursor-pointer py-3 px-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/") && "text-black border-b-green-700 bg-green-300"}`} onClick={()=>navigate("/")}>
+                        <li className={`cursor-pointer py-3 px-3 text-sm font-semibold text-pink-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/") && "text-white  text-[18px] border-b-pink-600 bg-pink-300"}`} onClick={()=>navigate("/")}>
                           Home
                         </li>
                         
-                        <li className={`cursor-pointer py-3 px-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/offers") && "text-black border-b-green-700 bg-green-300"}`}onClick={()=>navigate("/offers")}>
+                        <li className={`cursor-pointer py-3 px-3 text-sm font-semibold text-pink-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/offers") && "text-[18px] text-white border-b-pink-600 bg-pink-300"}`} onClick={()=>navigate("/offers")}>
                           Offers
                         </li>
                         
-                        <li className={`cursor-pointer py-3 px-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/signin") && "text-black border-b-green-700 bg-green-300"}`}onClick={()=>navigate("/signin")}>
+                        <li className={`cursor-pointer py-3 px-3 text-sm font-semibold text-pink-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/signin") && "text-[18px] text-white border-b-pink-600 bg-pink-300"}`} onClick={()=>navigate("/signin")}>
                           Signin
                         </li>
                         
--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,7 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Signin() {
+
+  const [formInfo, setFormInfo] = useState(
+    {
+      email: "",
+      password: "",
+    }
+    );
+
+    const {email, password} = formInfo;
+    
+    function onchange(e)
+    {
+      setFormInfo((prevState) =>
+      ({
+        ...prevState,
+        [e.target.id]: e.target.value,
+      }))
+    }
+
+    
   return (
-    <div>Sign in</div>
+    <section>
+      <h1 
+      className="mt-6 text-3xl font-bold text-center">Sign In</h1>
+      
+      <div 
+      className="flex flex-wrap items-center justify-center max-w-6xl px-6 py-12 mx-auto">
+        <div 
+        className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6 ">
+          <img 
+          src='/signin-bacground 1.jpg' 
+          alt="random" 
+          className='w-full'/>
+        </div>
+        
+        <div className="w-full md:w=[67%] lg:w-[40%] lg:ml-20">
+          <form>
+
+            <input 
+            className='w-full px-4 py-2 my-10 text-xl text-gray-700 transition ease-in-out bg-white border-gray-300 rounded' 
+            type='email' 
+            id='email' 
+            value={email} 
+            onChange={onchange} 
+            placeholder='Email address'/>
+            
+            <div>
+            <input 
+            className='w-full px-4 py-2 text-xl text-gray-700 transition ease-in-out bg-white border-gray-300 rounded' 
+            type='text' 
+            id='email' 
+            value={password} 
+            onChange={onchange} 
+            placeholder='Password'/>
+            </div>
+            
+          </form>
+        </div>
+        
+      </div>
+    </section>
   )
 }
